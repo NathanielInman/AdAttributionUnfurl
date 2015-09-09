28fc8889b1b82a2fb3041032312dfd80f6bdf7a4
Nathaniel Inman <nate041010@gmail.com>
2015-09-09 09:30:12 -0500

    Changed deploy script.

--- deploy_production.sh
+++ deploy_production.sh
@@ -1,4 +1,4 @@
 tar -czvf mweb.tar.gz *
-rsync -avhtz -e "ssh -i ~/.ssh/id_rsa" mweb.tar.gz ninman@54.197.161.200:./
-ssh -i ~/.ssh/id_rsa ninman@54.197.161.200 "mv mweb.tar.gz /srv;cd /srv;sudo -u www-data tar -zxvf mweb.tar.gz --overwrite;rm mweb.tar.gz;sudo restart node-httpserver"
+rsync -avhtz -e "ssh -i ~/.ssh/id_rsa" mweb.tar.gz ninman@54.166.101.180:./
+ssh -i ~/.ssh/id_rsa ninman@54.166.101.180 "mv mweb.tar.gz /srv;cd /srv;sudo -u www-data tar -zxvf mweb.tar.gz --overwrite;rm mweb.tar.gz;sudo restart node-httpserver"
 rm mweb.tar.gz
