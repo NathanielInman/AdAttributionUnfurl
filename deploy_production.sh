e4e839c0ef20837e62bf09352af77a85685e2949
Nathaniel Inman <nate041010@gmail.com>
2015-09-09 10:32:57 -0500

    finished deploy script.

--- deploy_production.sh
+++ deploy_production.sh
@@ -1,4 +1,4 @@
 tar -czvf proxy.tar.gz *
 rsync -avhtz -e "ssh -i ~/.ssh/id_rsa" proxy.tar.gz ninman@54.166.101.180:./
-ssh -i ~/.ssh/id_rsa ninman@54.166.101.180 "sudo chown www-data:www-data proxy.tar.gz;mv proxy.tar.gz /srv;cd /srv;sudo -u www-data tar -zxvf proxy.tar.gz --overwrite;rm proxy.tar.gz;sudo restart node-httpserver"
+ssh -i ~/.ssh/id_rsa ninman@54.166.101.180 "mv proxy.tar.gz /tmp;mv /tmp/proxy.tar.gz /srv;cd /srv;sudo -u www-data tar -zxvf proxy.tar.gz --overwrite;sudo -u www-data rm proxy.tar.gz;sudo service node-httpserver restart"
 rm proxy.tar.gz
