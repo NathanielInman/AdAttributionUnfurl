8154b3f121200ed19cf06f0073f0a2afe9b158e3
Nathaniel Inman <nate041010@gmail.com>
2015-09-09 09:32:07 -0500

    renamed the files.

--- deploy_production.sh
+++ deploy_production.sh
@@ -1,4 +1,4 @@
-tar -czvf mweb.tar.gz *
+tar -czvf proxy.tar.gz *
 rsync -avhtz -e "ssh -i ~/.ssh/id_rsa" mweb.tar.gz ninman@54.166.101.180:./
-ssh -i ~/.ssh/id_rsa ninman@54.166.101.180 "mv mweb.tar.gz /srv;cd /srv;sudo -u www-data tar -zxvf mweb.tar.gz --overwrite;rm mweb.tar.gz;sudo restart node-httpserver"
-rm mweb.tar.gz
+ssh -i ~/.ssh/id_rsa ninman@54.166.101.180 "mv proxy.tar.gz /srv;cd /srv;sudo -u www-data tar -zxvf proxy.tar.gz --overwrite;rm proxy.tar.gz;sudo restart node-httpserver"
+rm proxy.tar.gz
