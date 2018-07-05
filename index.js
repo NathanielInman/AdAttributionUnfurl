32f4bdedf316dfa2b71b58c43ad14698703e52a0
Nathaniel Inman <nate@theoestudio.com>
2018-07-05 13:56:48 -0500

    cleaned code up and removed deprecated deploy stuff

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var express = require('express'),
-    cors = require('cors');
-    app = express(),
-    request = require('request'),
+const express = require('express');
+const cors = require('cors');
+const request = require('request');
+
+const app = express();
 
 process.on('uncaughtException', function(err){
   console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~');
@@ -11,30 +12,30 @@ process.on('uncaughtException', function(err){
 
 app.use(cors());
 
-app.get('/', function(req, res) {
-  var url=''; //holds the passed url parameter
+app.get('/',(req,res)=>{
+  let url=''; //holds the passed url parameter
 
   // Reconstruct all of the queries to make it into one query. We are 
   // not dissecting the queries, the proxied destination servers process them
-  Object.keys(req.query).forEach(function(key){
+  Object.keys(req.query).forEach(key=>{
     if(key!=='url')url+='&'+key+'=';
     url+=req.query[key];
   });
-  
+
   // Make sure to separate the secure protocol library as well as start out with
   // validating that the query is acceptable
   if(url.indexOf('http')<0||!url.length){
-    res.status(400).send({ error: 'Url parameter is missing or invalid.'  });
+    res.status(400).send({error: 'Url parameter is missing or invalid.'});
   }else{
     request({
       followAllRedirects: true,
       url: url
-    }, function (error, response, body) {
-      var targetUrl = response.request.uri.href;
-      if(targetUrl.indexOf('https://play.google.com/store/apps/')>=0){
-        targetUrl = targetUrl.replace('https://play.google.com/store/apps/','market://');
-      } //end if
-      if (!error) {
+    }, (error, response, body)=>{
+      let find = 'https://play.google.com/store/apps/'
+          replace = 'market://',
+          targetUrl = response.request.uri.href.replace(find,replace);
+
+      if(!error){
         console.log('Request @'+(new Date()));
         console.log('-->'+targetUrl);
         res.redirect(targetUrl);
@@ -46,5 +47,4 @@ app.get('/', function(req, res) {
 });
 
 app.listen(5000);
-
 console.log('Listening on port 5000...');
--- lib/deploy_production.sh
+++
@@ -1,4 +0,0 @@
-tar -czvf proxy.tar.gz *
-rsync -avhtz -e "ssh -i ~/.ssh/id_rsa" proxy.tar.gz ninman@54.166.101.180:./
-ssh -i ~/.ssh/id_rsa ninman@54.166.101.180 "sudo mv proxy.tar.gz /srv;cd /srv;sudo tar -zxvf proxy.tar.gz --overwrite;sudo rm proxy.tar.gz;sudo service node-httpserver restart"
-rm proxy.tar.gz
